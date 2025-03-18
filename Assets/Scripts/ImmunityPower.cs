abe7e16019d939a84ed31e474b51c01fe62ed00c
sol9342 <solanag44@gmail.com>
2025-03-18 08:52:33 -0700

    Added immunity powerup with rotation

---
+++ Assets/Scripts/ImmunityPower.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class ImmunityPower : MonoBehaviour
+{
+    [SerializeField] private int rotateSpeed;
+
+    void Start()
+    {
+        rotateSpeed = 1;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+
+    //can add OnTriggerEnter for when player hits it?
+}
