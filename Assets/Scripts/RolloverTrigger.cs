f5d68b193020f517d1555c662bfdbb1853b37a72
sol9342 <solanag44@gmail.com>
2025-03-26 22:02:00 -0700

    Adding explosion particles on bike (wip)

--- Assets/Scripts/RolloverTrigger.cs
+++ Assets/Scripts/RolloverTrigger.cs
@@ -8,11 +8,17 @@ public class RolloverTrigget : MonoBehaviour
     private GameObject explosionParticlesInstance;
     private void OnTriggerStay(Collider other)
     {
-         explosionParticlesInstance = Instantiate(explosionParticles, other.transform.position, Quaternion.identity);
-        Destroy(explosionParticlesInstance.gameObject, 4);
+        //explosionParticlesInstance = Instantiate(explosionParticles, other.transform.position, Quaternion.identity);
+       // Destroy(explosionParticlesInstance.gameObject, 1);
         rb.linearVelocity = Vector3.zero;
         rb.AddTorque(new Vector3(0, 0, -flipUprightForce), ForceMode.Impulse);
-        explosionParticlesInstance = Instantiate(explosionParticles, other.transform.position, Quaternion.identity);
+        //explosionParticlesInstance = Instantiate(explosionParticles, other.transform.position, Quaternion.identity);
+        //Destroy(explosionParticlesInstance.gameObject, 1);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        explosionParticlesInstance = Instantiate(explosionParticles, gameObject.transform.position, Quaternion.identity);
         Destroy(explosionParticlesInstance.gameObject, 1);
     }
 }
